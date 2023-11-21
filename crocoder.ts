50cad50c337822fcc3c66883e7bb2d2de0e20703
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-11-20 23:38:38 -0300

    initials

--- crocoder.ts
+++ crocoder.ts
@@ -1,25 +1,33 @@
-
 //Array squared
 //Square the value of every element in the array. Presume that you will only get numbers in the input array.
 const inputSquared = [1, 2, 3, 4, 5];
-const map = inputSquared.map((x) => Math.pow(x, 2))
+const map = inputSquared.map((x) => Math.pow(x, 2));
 
-console.log("Array squared:", map)
+console.log("Array squared:", map);
 
 //Sum of every positive element
 //If the given input is an array of numbers, return the sum of all the positives ones. If the array is empty or there aren't any positive numbers, return 0.
 const inputFilter = [1, -4, 12, 0, -3, 29, -150];
-const filter = inputFilter.filter((positive) => positive > 0)
-console.log(filter)
+const filter = inputFilter.filter((positive) => positive > 0);
+console.log(filter);
 const initialValue = 0;
-const sumResult = filter.reduce((positive, current) => positive + current, initialValue)
-console.log("Sum of every positive element:", sumResult)
+const sumResult = filter.reduce(
+  (positive, current) => positive + current,
+  initialValue
+);
+console.log("Sum of every positive element:", sumResult);
 
 //Calculate median and mean
 //Calculate the mean and median values of the number elements from the input array.
 const inputMedianMean = [12, 46, 32, 64];
-inputMedianMean
-Math.abs(inputMedianMean)
-const median = ( ) => {
-    return Math.abs(inputMedianMean)
-}
+inputMedianMean.sort((a, b) => a - b);
+
+const inputInitial = "George Raymond Richard Martin";
+
+console.log(
+  "Get name initials",
+  inputInitial
+    .split(" ")
+    .map((word) => word[0])
+    .join("")
+);
